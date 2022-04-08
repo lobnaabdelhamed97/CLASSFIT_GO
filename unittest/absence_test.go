c3895bead931981704ddf7ee4acc9de4c473d95c
git1-at-293527253720 <eyad.ahmed.abdelfatah@gmail.com>
2022-04-09 01:48:44 +0200

    docker

--- unittest/absence_test.go
+++ unittest/absence_test.go
@@ -1,51 +1,51 @@
 package Unittest
 import (
-  "testing"
-  "fmt"
-  "github.com/gin-gonic/gin"
-  "CLASSFIT_GO/Controllers"
-  "CLASSFIT_GO/Config"
-  "net/http"
-  "net/http/httptest"
-  "github.com/jinzhu/gorm"
+	"github.com/lobnaabdelhamed97/CLASSFIT_GO/Config"
+	"github.com/lobnaabdelhamed97/CLASSFIT_GO/Controllers"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 func Test_absence(t *testing.T) {
-        // Switch to test mode so you don't get such noisy output
+	// Switch to test mode so you don't get such noisy output
 
-    gin.SetMode(gin.TestMode)
+	gin.SetMode(gin.TestMode)
 
-    // Setup your router, just like you did in your main function, and
-    // register your routes
-    var err error
+	// Setup your router, just like you did in your main function, and
+	// register your routes
+	var err error
 
-    Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
 	if err != nil {
 		fmt.Println("Status:", err)
 	}
 	defer Config.DB.Close()
-    r := gin.Default()
+	r := gin.Default()
 	r.GET("/absence", Controllers.GetAbsences)
 
-    // Create the mock request you'd like to test. Make sure the second argument
-    // here is the same as one of the routes you defined in the router setup
-    // block!
-    req, err := http.NewRequest(http.MethodGet, "/absence",nil)
-    if err != nil {
-        t.Fatalf("Couldn't create request: %v\n", err)
-    }
+	// Create the mock request you'd like to test. Make sure the second argument
+	// here is the same as one of the routes you defined in the router setup
+	// block!
+	req, err := http.NewRequest(http.MethodGet, "/absence", nil)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
 
-    // Create a response recorder so you can inspect the response
-    w := httptest.NewRecorder()
+	// Create a response recorder so you can inspect the response
+	w := httptest.NewRecorder()
 
-    // Perform the request
-    r.ServeHTTP(w, req)
-    fmt.Println(w.Body)
+	// Perform the request
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body)
 
-    // Check to see if the response was what you expected
-    if w.Code == http.StatusOK {
-        t.Logf("Expected to get status %d is same as %d\n", http.StatusOK, w.Code)
-        } else {
-        t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-    }
+	// Check to see if the response was what you expected
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same as %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
 
 }
