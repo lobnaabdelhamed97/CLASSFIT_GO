f6d10147107043d3b0878b0a5dcf010226642a27
Aydie-yasser <aydieyasser@gmail.com>
2022-04-12 10:30:46 +0200

    latest updates

--- Models/Game.go
+++ Models/Game.go
@@ -95,26 +95,21 @@ func (in *Input) Validate() error {
     Gm_id, _:= strconv.Atoi(in.Gm_id)
     PlyID, _:= strconv.Atoi(in.PlyID)
     if Gm_id  <= 0 {
-		return errors.New("Data Required")
+		return errors.New("GmID Required")
 	}
 	if PlyID <= 0 {
-		return errors.New("required Player ID")
+		return errors.New("PlayerID Required")
 	}
 	return nil
 }
 
 func Member_info(in *Input, mem_info *Mem_info) (err error) {
 
-	query := "SELECT distinct ply_id,ply_fname,ply_lname,country_name,city_name,contact_id,gm_ply_ply_id,guest_ply_id,CASE WHEN ply_city_sett = 'y' THEN 'true' ELSE 'false' END AS privecy,gm_ply_ply_id ,guest_ply_id  " +
-		"FROM players " +
-		"LEFT JOIN country ON ply_country_id= country_id " +
-		"LEFT JOIN city ON ply_city_id = city_id " +
-		"LEFT JOIN gm_players ON gm_ply_ply_id=ply_id and gm_ply_gm_id= " + in.Gm_id + " " +
-		"LEFT JOIN guests ON guest_ply_id=gm_ply_ply_id and guest_gm_id= " + in.Gm_id + " " +
-		"LEFT JOIN contacts ON contact_ply_id = ply_id and contact_org_id IN (SELECT gm_org_id from game WHERE gm_id=" + in.Gm_id + ") where ply_id=" + in.PlyID + ";"
-
-          if err = Config.DB.Raw(query).Scan(&mem_info).Error; err != nil {
-                return errors.New("No Available Data")
+  if err = Config.DB.Table("players").Select("ply_id, ply_fname ,ply_lname ,country_name, city_name ,contact_id,gm_ply_ply_id,guest_ply_id,CASE WHEN ply_city_sett = 'y' THEN 'true' ELSE 'false' END AS privecy").
+  Joins("LEFT JOIN country ON ply_country_id= country_id").Joins("LEFT JOIN city ON ply_city_id = city_id").Joins("LEFT JOIN gm_players ON gm_ply_ply_id=ply_id and gm_ply_gm_id= " + in.Gm_id + " ").
+  Joins("LEFT JOIN guests ON guest_ply_id=gm_ply_ply_id and guest_gm_id= " + in.Gm_id + " ").Joins("LEFT JOIN contacts ON contact_ply_id = ply_id and contact_org_id IN (SELECT gm_org_id from game WHERE gm_id= " + in.Gm_id + ") ").Where("ply_id= "+in.PlyID+" ").
+  Scan(&mem_info).Error; err != nil {
+        return errors.New("No Available Data")
         }
           if mem_info.Gm_ply_ply_id > 0 && mem_info.Guest_ply_id == 0 {
                 mem_info.PlyType = "member"
